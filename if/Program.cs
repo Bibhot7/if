c417f5b8ba8a8b4349dba56b4c4c0d388864609d
Bibhot7 <bibhot7@outlook.com>
2024-01-27 07:07:38 +1300

    Add project files.

---
+++ if/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace iszaf;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //if statements practice.
+        Console.WriteLine( "what is temperature like?");
+        string temperature =  Console.ReadLine();   
+        //conversing string into num
+        int numTemp = int.Parse(temperature);
+
+        if (numTemp < 20)
+        {
+            Console.WriteLine("its less than 20 degrees outside");
+        }
+
+        else if (numTemp == 20)
+        {
+            Console.WriteLine( "temperature is exactly 20 degrees");
+        }
+
+        else (numTemp > 20)
+        {
+            Console.WriteLine( "it kinda hot outside");
+        }
+    }
+}
